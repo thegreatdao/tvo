9f5acc165c5cb64726b792852e94912349bb4604
HL <anotherdayinpara@yahoo.com>
2010-04-06 12:21:01 -0400

    renamed TvoScheduler.java

--- src/main/java/com/tvo/scheduler/TvoScheduler.java
+++ src/main/java/com/tvo/scheduler/TvoScheduler.java
@@ -4,7 +4,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TvoMainScheduler
+public class TvoScheduler
 {
 	@Scheduled(fixedDelay=5000)
 	public void print()
