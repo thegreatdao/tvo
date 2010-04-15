3ec15e1235fbe8aa6bf642ae72f0156c2fee1c34
HL <anotherdayinpara@yahoo.com>
2010-04-15 16:40:20 -0400

    refactored AssetArticle.java

--- src/main/java/com/tvo/entity/AssetArticle.java
+++ src/main/java/com/tvo/entity/AssetArticle.java
@@ -1,9 +1,11 @@
 package com.tvo.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class AssetArticle implements java.io.Serializable
+@EqualsAndHashCode(callSuper=true)
+public class AssetArticle extends TvoEntity
 {
 
 	private static final long serialVersionUID = -9066260942514094862L;
@@ -11,5 +13,6 @@ public class AssetArticle implements java.io.Serializable
 	private int assetArticleId;
 	private String teaser;
 	private String body;
+	private AssetRoot assetRoot;
 
 }
