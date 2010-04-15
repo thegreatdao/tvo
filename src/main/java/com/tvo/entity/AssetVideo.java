14bff05467f7a76e3b3149dd4c0bdfe7bfc85d75
HL <anotherdayinpara@yahoo.com>
2010-04-15 16:25:10 -0400

    optimize imports to AssetVideo.java

--- src/main/java/com/tvo/entity/AssetVideo.java
+++ src/main/java/com/tvo/entity/AssetVideo.java
@@ -1,7 +1,5 @@
 package com.tvo.entity;
 
-import java.util.List;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
