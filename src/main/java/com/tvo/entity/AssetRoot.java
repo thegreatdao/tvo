f695552db3b953a9b820ac2d44384cf0be0c73a4
Ronald <rond@ronald-douglas.com>
2010-04-21 13:57:51 -0400

    Added type safety for asset types

--- src/main/java/com/tvo/entity/AssetRoot.java
+++ src/main/java/com/tvo/entity/AssetRoot.java
@@ -11,7 +11,9 @@ import javax.xml.bind.annotation.XmlType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import org.sakaiproject.genericdao.api.annotations.PersistentColumnName;
 import org.sakaiproject.genericdao.api.annotations.PersistentId;
+import org.sakaiproject.genericdao.api.annotations.PersistentTransient;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
@@ -19,42 +21,46 @@ import org.sakaiproject.genericdao.api.annotations.PersistentId;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class AssetRoot extends TvoEntity
 {
-	
 	private static final long serialVersionUID = 3386949100542034153L;
-
+	
+	public enum AssetType
+	{
+		PROMO, SERIES, PROGRAM, VIDEO, ARTICLE, BLOG, GAME, CONTEST, NA
+	}
+	
+	@PersistentTransient
+	public AssetType getAssetType()
+	{
+		return AssetType.valueOf(this.asset_Type);
+	}
+	
+	public void setAssetType(AssetType typeCategory)
+	{
+		this.asset_Type = typeCategory.toString();
+	}
+	
 	@PersistentId
 	@XmlTransient
 	private Integer assetRootId;
-	@XmlElement(name="geo_id")
 	private Integer geoFilterId;
-	@XmlElement(name="taid")
 	private String telescopeAssetId;
-	@XmlElement(name="trId")
 	private Integer telescopeRecordId;
 	private String title;
 	private String source;
+	
 	@XmlElement(name="type")
-	private String assetType;
-	@XmlElement(name="DI")
+	@PersistentColumnName("asset_type")
+	private String asset_Type;
+	
 	private String descriptionInternet;
-	@XmlElement(name="DESC")
 	private String descriptionShort;
-	@XmlElement(name="AGE_RATE")
 	private String ageRating;
-	@XmlElement(name="user_time_start")
 	private Date userTimeStart;
-	@XmlElement(name="user_time_end")
 	private Date userTimeEnd;
 	private Date duration;
-	@XmlElement(name="release_date")
 	private Date releaseDate;
-	@XmlElement(name="created_on")
 	private Date createdOn;
-	@XmlElement(name="updated_on")
 	private Date updatedOn;
-	@XmlElement(name="created_by")
 	private String createdBy;
-	@XmlElement(name="updated_by")
 	private String updatedBy;
-
 }
