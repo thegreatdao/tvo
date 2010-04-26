b55b83b37fc1c5d2236284c28465e0a0d67aad48
HL <anotherdayinpara@yahoo.com>
2010-04-26 15:36:42 -0400

    reformat rs format

--- src/main/java/com/tvo/entity/AssetRoot.java
+++ src/main/java/com/tvo/entity/AssetRoot.java
@@ -42,8 +42,11 @@ public class AssetRoot extends TvoEntity
 	@PersistentId
 	@XmlTransient
 	private Integer assetRootId;
+	@XmlElement(name="geo_id")
 	private Integer geoFilterId;
+	@XmlElement(name="taid")
 	private String telescopeAssetId;
+	@XmlElement(name="trId")
 	private Integer telescopeRecordId;
 	private String title;
 	private String source;
@@ -52,15 +55,25 @@ public class AssetRoot extends TvoEntity
 	@PersistentColumnName("asset_type")
 	private String asset_Type;
 
+	@XmlElement(name="DI")
 	private String descriptionInternet;
+	@XmlElement(name="DESC")
 	private String descriptionShort;
+	@XmlElement(name="AGE_RATE")
 	private String ageRating;
+	@XmlElement(name="user_time_start")
 	private Date userTimeStart;
+	@XmlElement(name="user_time_end")
 	private Date userTimeEnd;
 	private Date duration;
+	@XmlElement(name="release_date")
 	private Date releaseDate;
+	@XmlElement(name="created_on")
 	private Date createdOn;
+	@XmlElement(name="updated_on")
 	private Date updatedOn;
+	@XmlElement(name="created_by")
 	private String createdBy;
+	@XmlElement(name="updated_by")
 	private String updatedBy;
 }
--- src/main/java/com/tvo/entity/AssetVideo.java
+++ src/main/java/com/tvo/entity/AssetVideo.java
@@ -24,12 +24,14 @@ public class AssetVideo extends TvoEntity
 	private static final long serialVersionUID = 6049312423966660328L;
 	
 	@PersistentId
-	@XmlElement(name="video_id")
+	@XmlElement(name="id")
 	private Integer assetVideoId;
 	@XmlTransient
 	private Integer assetRootId;
 	private Integer length;
+	@XmlElement(name="link_url")
 	private String linkUrl;
+	@XmlElement(name="link_title")
 	private String linkTitle;
 	@XmlElement(name="msn")
 	private String masterSeriesNumber;
@@ -39,7 +41,9 @@ public class AssetVideo extends TvoEntity
 	private String thumbnailUrl;
 	@XmlElement(name="videostill_url")
 	private String videoStillUrl;
+	@XmlElement(name="video_url")
 	private String videoUrl;
+	@XmlElement(name="bc_ref_id")
 	private String bcRefId;
 	@PersistentTransient
 	@XmlElement(name="root")
