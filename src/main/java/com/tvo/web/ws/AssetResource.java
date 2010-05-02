d14edcdf9b1d2cc7ddfa557f2de629df5631e05e
hongli li <anotherdayinpara@yahoo.com>
2010-05-01 22:58:33 -0400

    added Dao to retrived assetVideos by data range and expose it as webservices

--- src/main/java/com/tvo/web/ws/AssetResource.java
+++ src/main/java/com/tvo/web/ws/AssetResource.java
@@ -1,8 +1,13 @@
 package com.tvo.web.ws;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +15,38 @@ import org.springframework.stereotype.Controller;
 
 import com.tvo.entity.AssetRoot;
 import com.tvo.entity.AssetVideo;
+import com.tvo.entity.wrapper.AssetVideos;
+import com.tvo.service.AssetVideoService;
 import com.tvo.service.TvoAssetsService;
 
 @Controller
 @Path(AssetResource.ASSET_ROOT_URL)
 public class AssetResource
 {
-	public static final String ASSET_ROOT_URL ="/ws/assetRoot";
+	public static final String ASSET_ROOT_URL ="/ws/assets";
 	
 	@Autowired
 	private TvoAssetsService tvoAssetsService;
+	@Autowired
+	private AssetVideoService assetVideoService;
 	
 	@GET
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-	@Path("data")
+	@Path("videos")
 	public AssetVideo getAssetVideo()
 	{
 		AssetRoot assetRoot = tvoAssetsService.findAll(AssetRoot.class).get(0);
 		AssetVideo assetVideo = tvoAssetsService.findAll(AssetVideo.class).get(0);
 		assetVideo.setAssetRoot(assetRoot);
 		return assetVideo;
+	}
+	
+	@GET
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Path("videos/{startDate}/{endDate}")
+	public AssetVideos getAssetVideosByDates(@PathParam("startDate") String startDate, @PathParam("endDate") String endDate)
+	{
+		List<AssetVideo> assetVideos = assetVideoService.findAssetVideosByDates(startDate, endDate);
+		return new AssetVideos(assetVideos);
 	}
 }
