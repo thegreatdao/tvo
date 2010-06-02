26cf192cd3858f0f7e73440fbf30d4a99d8c756e
U-Ronald-Portable\Ronald <Ronald@Ronald-Portable.(none)>
2010-06-01 23:16:21 -0400

    strands

--- src/main/java/com/tvo/telescope/TelescopeStrandsRowMapper.java
+++ src/main/java/com/tvo/telescope/TelescopeStrandsRowMapper.java
@@ -2,48 +2,46 @@ package com.tvo.telescope;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import org.springframework.jdbc.core.RowMapper;
 
+import com.tvo.entity.StrandsScheduleView;
+
 public class TelescopeStrandsRowMapper implements RowMapper {
+	
 	public Object mapRow(ResultSet rs, int line) throws SQLException
 	{
+		StrandsScheduleView ssv = new StrandsScheduleView();
 
-		assetRoot.setId(rs.getInt("asset_root_id"));
-		assetRoot.setTelescopeAssetId(rs.getString("r_telescope_asset_id"));
-		assetRoot.setTelescopeRecordId(rs.getInt("r_telescope_record_id"));
-		assetRoot.setTitle(rs.getString("r_title"));
-		assetRoot.setSource(rs.getString("r_source"));
-		assetRoot.setTypeCategory(AssetRoot.AssetType.valueOf(rs
-				.getString("r_type")));
-		assetRoot
-				.setDescriptionInternet(rs.getString("r_description_internet"));
-		assetRoot.setDescriptionShort(rs.getString("r_description_short"));
-
-		assetRoot.setAgeRating(rs.getString("r_age_rating"));
-		assetRoot.setUserTimeStart(rs.getDate("r_user_time_start"));
-		assetRoot.setUserTimeEnd(rs.getDate("r_user_time_end"));
-		assetRoot.setDuration(rs.getTime("r_duration"));
-
-		assetRoot.setReleaseDate(rs.getDate("r_release_date"));
-		assetRoot.setCreatedOn(rs.getTimestamp("r_created_on"));
-		assetRoot.setUpdatedOn(rs.getTimestamp("r_updated_on"));
-		assetRoot.setCreatedby(rs.getString("r_created_by"));
-		assetRoot.setUpdatedBy(rs.getString("r_updated_by"));
-
-		return assetRoot;
+		ssv.setIdSeries(rs.getString("id_series"));
+		ssv.setSeriesTitle(rs.getString("series_title"));
+		ssv.setSeriesDescription(rs.getString("series_description"));
+		ssv.setBpn(rs.getString("bpn"));
+		ssv.setProgramTitle(rs.getString("program_title"));
+		ssv.setProgramDescription(rs.getString("program_description"));
+		ssv.setLongDescription(rs.getString("long_description"));
+		ssv.setDurationSeconds(rs.getInt("duration_seconds"));
+		ssv.setAiringTime(rs.getDate("airing_time"));
+		ssv.setNetwork(rs.getString("network"));
+		ssv.setVRating(rs.getString("v_rating"));
+		ssv.setWeekday(rs.getString("weekday"));
+		ssv.setIsWeekend(rs.getString("is_weekend"));
+		ssv.setIsRepeat(rs.getString("is_repeat"));
+		ssv.setIsFirstAir(rs.getString("is_first_air"));
+		ssv.setIsCaptioned(rs.getString("is_captioned"));
+		ssv.setIsDescVideo(rs.getString("is_desc_video"));
+		ssv.setAudienceDescretion(rs.getString("audience_descretion"));
+		ssv.setYearProduced(rs.getString("year_produced"));
+		ssv.setEpisodeNumber(rs.getInt("episode_number"));
+		ssv.setEpisodeOrder(rs.getString("episoder_order"));
+		ssv.setPreviousAir(rs.getDate("previous_air"));	
+		ssv.setDefSeriesTitle(rs.getString("def_series_title"));
+		ssv.setDefSeriesDescription(rs.getString("def_series_description"));
+		ssv.setDefProgramTitle(rs.getString("def_program_title"));
+		ssv.setDefProgramDescription(rs.getString("def_program_description"));
+		ssv.setDefLongDescription(rs.getString("def_long_description"));
+		
+		return ssv;
 	}
-}
-
-
-
-
-
-@SuppressWarnings("unchecked")
-public class AssetRootRowMapper implements RowMapper
-{
-
-	protected AssetRoot	assetRoot;
-
-	
 }
--- src/main/java/com/tvo/telescope/TelescopeStrandsSchedule.java
+++ src/main/java/com/tvo/telescope/TelescopeStrandsSchedule.java
@@ -1,5 +1,25 @@
 package com.tvo.telescope;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.tvo.databases.DbWebRepository;
+import com.tvo.entity.StrandsScheduleView;
+
 public class TelescopeStrandsSchedule {
-	
+	@SuppressWarnings("unchecked")
+	public StrandsScheduleView[] getSchedule(int daysAhead) {
+		
+		JdbcTemplate select = new JdbcTemplate();
+		select.setDataSource(DbWebRepository.getDataSource());
+		List<StrandsScheduleView> strandsScheduleList = select.query(
+		"SELECT * FROM tsp.strand_listings_v " +
+        "WHERE airing_time BETWEEN TRUNC(sysdate) AND TRUNC(sysdate + ?) " +
+        "order by airing_time",
+		new Object[] { daysAhead }, new TelescopeStrandsRowMapper());
+		
+		StrandsScheduleView[] strandsScheduleArray = new StrandsScheduleView[strandsScheduleList.size()];
+		
+		return strandsScheduleArray;
+	}
 }
