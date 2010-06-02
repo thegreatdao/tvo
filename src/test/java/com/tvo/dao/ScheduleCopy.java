88de89b74fcdecfe04e902d7f74dbb6b96572625
Ronald <Ronald@blackfridge.(none)>
2010-06-02 12:20:15 -0400

    modified

--- src/test/java/com/tvo/dao/ScheduleCopy.java
+++ src/test/java/com/tvo/dao/ScheduleCopy.java
@@ -2,6 +2,7 @@ package com.tvo.dao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -16,13 +17,13 @@ import com.tvo.telescope.TelescopeStrandsSchedule;
 @Transactional
 public class ScheduleCopy {
 	
+	@Autowired
 	private StrandsScheduleViewServiceImpl strandsScheduleService;
 	
 	@Test
 	public void testAssetRootWithAssetVideo()
 	{
 		TelescopeStrandsSchedule strandsSchedule = new TelescopeStrandsSchedule();
-		
 		StrandsScheduleView[] ssList = strandsSchedule.getSchedule(4);
 		strandsScheduleService.update(ssList);	
 	}
